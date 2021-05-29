5060c0248f41af5c4c0a53ee64e1bebadf231946
BKT <umeshanuc@gmail.com>
2021-05-29 17:29:21 +0530

    Initialize

---
+++ HospitalWebAPI/Database/Dbase.cs
@@ -0,0 +1,19 @@
+﻿using HospitalWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalWebAPI.Database
+{
+    public class Dbase : DbContext
+    {
+        public Dbase(DbContextOptions<Dbase> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Doctor> Doctor_Set { get; set; }
+    }
+}
